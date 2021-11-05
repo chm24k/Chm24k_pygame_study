4cb01b0c3babf13e6216b53fc8570feb003155ca
chm24k <chm24k@gmail.com>
2021-11-05 16:05:53 +0900

    Tictactoe - 틱택토(GUI __init__ )

---
+++ Python/TicTacToe/tictactoe_GUI.py
@@ -0,0 +1,15 @@
+from tictactoe_gameengine import TictactoeGameEngine
+class TictactoeGUI:
+    def __init__(self):
+        self.game_engine = TictactoeGameEngine()
+
+    def init_GUI(self): #화면구성하는거 넣을거임
+        pass
+
+    def click_handler(self, evnet): #클릭처리기  - event넣어짐
+        pass
+
+if __name__ == '__main__':
+    ttt_GUI = TictactoeGUI()
+
+
